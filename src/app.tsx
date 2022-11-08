753f68099d74580bd278d8a4557dd6ed922b89e9
Rupert McKay <rupert.mckay@lokalise.com>
2022-11-08 12:20:59 +0100

    as a hook now

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const FileTable = ({ children: files }: { children: File[] }) =>
 	files.length === 0 ? (
@@ -26,13 +26,39 @@ const FileTable = ({ children: files }: { children: File[] }) =>
 		</table>
 	);
 
+const useDropZone = <T extends Element>({
+	onDrop,
+}: {
+	onDrop: (dragEvent: React.DragEvent<T>) => unknown;
+}) => {
+	const [isDragActive, setIsDragActive] = useState(false)
+
+	return {
+		getProps: () => ({
+			onDrop: (e: React.DragEvent<T>) => {
+				e.preventDefault();
+				setIsDragActive(false);
+				onDrop(e);
+			},
+			onDragOver: (e: React.DragEvent<T>) => {
+				e.preventDefault();
+				e.stopPropagation();
+			},
+			onDragEnter: () => { setIsDragActive(true) },
+			onDragLeave: () => { setIsDragActive(false) },
+		}),
+		isDragActive
+	}
+};
+
 export const App = () => {
 	const [files, setFiles] = useState<File[]>([]);
 
 	const onDrop = (dragEvent: React.DragEvent) => {
-		dragEvent.preventDefault();
 		setFiles((f) => [...f, ...dragEvent.dataTransfer.files]);
 	};
+
+	const { isDragActive, getProps } = useDropZone({ onDrop });
 
 	return (
 		<main className="stack center">
@@ -43,15 +69,8 @@ export const App = () => {
 				<a href="https://github.com/rupert-mckay/drop-input">drop-input</a>
 			</p>
 			<label
-				style={{ minHeight: "10rem", border: "1px solid black" }}
-				onDrop={(e) => {
-					e.preventDefault();
-					onDrop(e);
-				}}
-				onDragOver={(e) => {
-					e.preventDefault();
-					e.stopPropagation();
-				}}
+				style={{ minHeight: "10rem", border: isDragActive ? "5px solid red" : "1px dashed black" }}
+				{...getProps()}
 			>
 				Drop anywhere in this zone!
 				<input style={{ display: "block" }} type="file" onDrop={onDrop} />
